7a83d8d2ccd0fad6b3a70f253a46a73cb2962610
ivanmolcan <ico@molcan.sk>
2021-03-22 22:19:31 +0100

    22.3.21 -1

--- Udemy/Rahul/Projects/ExcelDriven/src/test/java/sk/ivanmolcan/DataDriven.java
+++ Udemy/Rahul/Projects/ExcelDriven/src/test/java/sk/ivanmolcan/DataDriven.java
@@ -1,2 +1,54 @@
-package sk.ivanmolcan;public class DataDriven {
+package sk.ivanmolcan;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class DataDriven {
+
+    public static void main(String[] args) throws IOException {
+
+        //fileInputStream argument
+
+        FileInputStream fis = new FileInputStream("D:\\Doc\\Programovanie\\Automation Testing\\Udemy\\Rahul\\Projects\\ExcelDriven\\ExcelDriven.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+        int sheets = workbook.getNumberOfSheets();
+        for (int i = 0; i < sheets; i++) {
+            if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
+                XSSFSheet sheet = workbook.getSheetAt(i);
+
+                //Identifikovanie column
+                Iterator<Row> rows = sheet.iterator();
+                Row firstRow = rows.next();
+                Iterator<Cell> cell = firstRow.cellIterator();
+                int k = 0;
+                int column = 0;
+                while (cell.hasNext()) {
+                    Cell value = cell.next();
+                    if (value.getStringCellValue().equalsIgnoreCase("Testcases")) {
+                        column = k;
+                    }
+                    k++;
+                }
+                //Ked je identifikovany column scanujem cely 'Testcase' stlpec aby som nasiel riadok 'Purchase'
+                while (rows.hasNext()) {
+                    Row r = rows.next();
+                    if (r.getCell(column).getStringCellValue().equalsIgnoreCase("Purchase")) {
+                        Iterator<Cell> cv = r.cellIterator();
+                        while (cv.hasNext()) {
+                            System.out.println(cv.next().getStringCellValue());
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- Udemy/Rahul/Projects/ExtentReports/src/test/java/sk/ivanmolcan/ExtentReportDemo.java
+++ Udemy/Rahul/Projects/ExtentReports/src/test/java/sk/ivanmolcan/ExtentReportDemo.java
@@ -1,2 +1,48 @@
-package sk.ivanmolcan;public class ExtentReportDemo {
+package sk.ivanmolcan;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ExtentReportDemo {
+
+    ExtentReports extent;
+
+    @BeforeTest
+    public void config() {
+        String path = System.getProperty("user.dir") + "\\reports\\index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Ivan's tests");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Ivan M.");
+
+    }
+
+    @Test
+    public void initialDemo() {
+
+//        extent.createTest("Initial Demo");
+        ExtentTest test = extent.createTest("Initial Demo");
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Doc\\Programovanie\\Automation Testing\\Materials\\Webdriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://rahulshettyacademy.com");
+
+        System.out.println(driver.getTitle());
+        driver.close();
+        test.fail("Result to not match");
+
+        extent.flush();
+
+    }
 }
